1f3cebdd898dbcea46ac477289e8b6f258a1fe30
asaco <asqen4101@gmail.com>
2018-02-24 16:13:06 +0900

    commits

---
+++ test2017-10-21/practice.py
@@ -0,0 +1,7 @@
+fmt = "俺は{name}。{job}をやって{hobby}を楽しむ{place}在住{age}歳だ。"
+G = fmt.format(name="黒眼鏡", job="プログラマー", hobby="DJ", place="TOKYO", age="26")
+print(G)
+
+
+#分解してみる
+#print("俺は{namae}。{nenrei}と高齢だが、アレがアレで{are}をやっている。".format(namae="丹古母鬼馬二", nenrei=80, are="プログラマ"))
